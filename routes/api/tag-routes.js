757a0739739347a41730440f36efac83655bea46
mich-hales <michellehales93@gmail.com>
2022-10-17 11:08:10 -0600

    routes updated, database seeded

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -58,8 +58,20 @@ router.put('/:id', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(`id:${req.params.id} has been deleted`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
