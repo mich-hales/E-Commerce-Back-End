f2af9eddb3aaf159fa6677d40dbbc1b442226de5
mich-hales <michellehales93@gmail.com>
2022-10-17 10:56:14 -0600

    put route updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,12 +29,33 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then(() => {
+    res.json(`id:${req.params.id} has been updated`);
+  }).catch((err) => {
+    res.json(err);
+  });
+
 });
 
 router.delete('/:id', (req, res) => {
